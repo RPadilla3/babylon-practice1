aa4384baa3773ee2afbc46e315205a1d74db8bb5
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-01-25 16:30:07 -0500

    Created a toggle function for the light in the scene to turn on and off on the click of the space button.

--- babylon/babylon-start.js
+++ babylon/babylon-start.js
@@ -5,13 +5,25 @@ window.addEventListener('DOMContentLoaded', function() {
       var createScene = function() {
         var scene = new BABYLON.Scene(engine);
         scene.clearColor = new BABYLON.Color3.White();
-        var box = BABYLON.Mesh.CreateBox('Box', 4.0, scene)
+        var material = new BABYLON.StandardMaterial('material1', scene);
+        material.wireframe = true;
+        var box = BABYLON.Mesh.CreateBox('Box', 4.0, scene);
 
         var camera = new BABYLON.ArcRotateCamera('arcCamera',
           BABYLON.Tools.ToRadians(45),
           BABYLON.Tools.ToRadians(45),
           10.0, box.position, scene);
         camera.attachControl(canvas, true);
+
+        var light = new BABYLON.PointLight('pointlight', new BABYLON.Vector3(0,10,0), scene);
+        light.diffuse = new BABYLON.Color3(1,0,0);
+
+        scene.actionManager = new BABYLON.ActionManager(scene);
+        scene.actionManager.registerAction(
+          new BABYLON.ExecuteCodeAction({trigger: BABYLON.ActionManager.OnKeyUpTrigger, parameter: " "},
+          function() {
+            light.setEnabled(!light.isEnabled());
+          }));
 
         return scene;
       }
