9b38a7d16234b6c3c629feaeca0f35734dc09866
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-01-25 16:03:33 -0500

    Changed the camera to an ArcCamera for more fluent views.

--- babylon/babylon-start.js
+++ babylon/babylon-start.js
@@ -1,5 +1,3 @@
-
-
 window.addEventListener('DOMContentLoaded', function() {
       var canvas = document.getElementById('canvas');
       var engine = new BABYLON.Engine(canvas, true);
@@ -7,10 +5,14 @@ window.addEventListener('DOMContentLoaded', function() {
       var createScene = function() {
         var scene = new BABYLON.Scene(engine);
         scene.clearColor = new BABYLON.Color3.White();
-        var camera = new BABYLON.FreeCamera('camera1',
-         new BABYLON.Vector3(0,6,-10), scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
         var box = BABYLON.Mesh.CreateBox('Box', 4.0, scene)
+
+        var camera = new BABYLON.ArcRotateCamera('arcCamera',
+          BABYLON.Tools.ToRadians(45),
+          BABYLON.Tools.ToRadians(45),
+          10.0, box.position, scene);
+        camera.attachControl(canvas, true);
+
         return scene;
       }
 
