a99b73a8dff6913fdade5c25e863161c739a709b
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-01-26 17:15:23 -0500

    Added more shapes and stretched the ground moving all objects onto the ground. changed the animation to the torus knot.

--- babylon/babylon-start.js
+++ babylon/babylon-start.js
@@ -8,19 +8,30 @@ window.addEventListener('DOMContentLoaded', function() {
       scene.clearColor = new babylon.Color3.White();
     var material = new babylon.StandardMaterial('material1', scene);
       material.wireframe = true;
-    var ground = babylon.Mesh.CreateGround('ground', 10,20,6, scene);
+    var ground = babylon.Mesh.CreateGround('ground', 100,50,6, scene);
     var box = babylon.Mesh.CreateBox('Box', 4, scene);
+      box.position.y = 2;
     var box2 = babylon.Mesh.CreateBox('Box1', 4, scene);
-      box2.position.y = 5;
+      box2.position.y = 10;
     var fountain = babylon.Mesh.CreateBox('fountain', 2, scene);
       fountain.position.x = 20;
+      fountain.position.y = 2;
     var sphere = babylon.Mesh.CreateSphere('sphere', 10, 4, scene);
       sphere.position.x = 7.5;
+      sphere.position.y = 2;
     var cone = babylon.Mesh.CreateCylinder('cone', 3,0,3,20, true, scene);
       cone.position.z = 15;
+      cone.position.y = 2;
     var cone2 = babylon.Mesh.CreateCylinder('cone2', 3,3,0,20, true, scene);
       cone2.position.z = 15;
       cone2.position.y = 3;
+    var torus = babylon.Mesh.CreateTorus('torus', 5,1,10, scene);
+      torus.position.y = 2;
+      torus.position.z = 20;
+    var knot = babylon.Mesh.CreateTorusKnot('knot', 3,1,1000,80,2,3, scene);
+      knot.position.x = 30;
+      knot.position.y = 6;
+
     var camera = new babylon.ArcRotateCamera('arcCamera',
       babylon.Tools.ToRadians(45),
       babylon.Tools.ToRadians(45),
@@ -47,12 +58,12 @@ window.addEventListener('DOMContentLoaded', function() {
    });
 
    animation.setKeys(keys);
-   fountain.animations.push(animation);
-   scene.beginAnimation(fountain, 0, 100, true);
+   knot.animations.push(animation);
+   scene.beginAnimation(knot, 0, 100, true);
 
 
     var light = new babylon.PointLight('pointlight', new babylon.Vector3(0,50,0), scene);
-      light.diffuse = new babylon.Color3(1,2,0);
+      light.diffuse = new babylon.Color3.FromHexString('#1f7979');
 
     scene.actionManager = new babylon.ActionManager(scene);
     scene.actionManager.registerAction(
