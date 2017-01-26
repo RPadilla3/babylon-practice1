cf75404dd8b4649b3f1ee32482e1aecdc72237fc
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-01-26 10:04:13 -0500

    created a babylon variable so that no one has to type out BABYLON for every instance.

--- babylon/babylon-start.js
+++ babylon/babylon-start.js
@@ -1,35 +1,40 @@
 window.addEventListener('DOMContentLoaded', function() {
-      var canvas = document.getElementById('canvas');
-      var engine = new BABYLON.Engine(canvas, true);
+  var babylon = BABYLON;
+  var canvas = document.getElementById('canvas');
+  var engine = new babylon.Engine(canvas, true);
 
-      var createScene = function() {
-        var scene = new BABYLON.Scene(engine);
-        scene.clearColor = new BABYLON.Color3.White();
-        var material = new BABYLON.StandardMaterial('material1', scene);
-        material.wireframe = true;
-        var box = BABYLON.Mesh.CreateBox('Box', 4.0, scene);
+  var createScene = function() {
+    var scene = new babylon.Scene(engine);
+    scene.clearColor = new babylon.Color3.White();
+    var material = new babylon.StandardMaterial('material1', scene);
+    material.wireframe = true;
+    var box = babylon.Mesh.CreateBox('Box', 4.0, scene);
 
-        var camera = new BABYLON.ArcRotateCamera('arcCamera',
-          BABYLON.Tools.ToRadians(45),
-          BABYLON.Tools.ToRadians(45),
-          10.0, box.position, scene);
-        camera.attachControl(canvas, true);
+    var camera = new babylon.ArcRotateCamera('arcCamera',
+    babylon.Tools.ToRadians(45),
+    babylon.Tools.ToRadians(45),
+    10.0, box.position, scene);
+    camera.attachControl(canvas, true);
 
-        var light = new BABYLON.PointLight('pointlight', new BABYLON.Vector3(0,10,0), scene);
-        light.diffuse = new BABYLON.Color3(1,0,0);
+    var light = new babylon.PointLight('pointlight', new babylon.Vector3(0,10,0), scene);
+         light.diffuse = new babylon.Color3(1,0,0);
 
-        scene.actionManager = new BABYLON.ActionManager(scene);
-        scene.actionManager.registerAction(
-          new BABYLON.ExecuteCodeAction({trigger: BABYLON.ActionManager.OnKeyUpTrigger, parameter: " "},
-          function() {
-            light.setEnabled(!light.isEnabled());
-          }));
+         scene.actionManager = new babylon.ActionManager(scene);
+         scene.actionManager.registerAction(
+           new babylon.ExecuteCodeAction({trigger: babylon.ActionManager.OnKeyUpTrigger, parameter: " "},
+           function() {
+             light.setEnabled(!light.isEnabled());
+           }));
 
-        return scene;
-      }
+    return scene;
+  }
 
-      var scene = createScene();
-      engine.runRenderLoop(function() {
-        scene.render();
-      });
+  var scene = createScene();
+  engine.runRenderLoop(function() {
+    scene.render();
+  });
+
+  window.addEventListener('resize', function() {
+    engine.resize();
+  });
 });
